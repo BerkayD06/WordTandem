12b78e0f2e76da2ca0161709d628c8cba5ae48ba
BerkayDemirbaş <berkaydemirbas@saatteknoloji.com>
2025-03-17 20:09:13 +0300

    initial commit

---
+++ src/main/java/com/berkayd/WordTandem/WordTandemApplication.java
@@ -0,0 +1,13 @@
+package com.berkayd.WordTandem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordTandemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordTandemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/berkayd/WordTandem/WordTandemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berkayd.WordTandem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordTandemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
